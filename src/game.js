3794faba1f246dc0cd21677df117cd64b7a66b7d
Taron Yeghoyan <taron_yeghoyan17@alumni.aua.am>
2019-06-29 19:24:00 +0400

    updates

--- src/game.js
+++ src/game.js
@@ -37,34 +37,47 @@ export default class Game {
   movePieceLeft() {
     this.activePiece.x -= 1;
 
-    if(this.isPieceOutOfBounds()) {
+    if (this.isPieceOutOfBounds()) {
       this.activePiece.x += 1;
     }
   }
 
+
   movePieceRight() {
     this.activePiece.x += 1;
 
-    if(this.isPieceOutOfBounds()) {
+    if (this.isPieceOutOfBounds()) {
       this.activePiece.x -= 1;
     }
   }
 
+
   movePieceDown() {
     this.activePiece.y += 1;
 
-    if(this.isPieceOutOfBounds()) {
+    if (this.isPieceOutOfBounds()) {
       this.activePiece.y -= 1;
     }
   }
 
+
   isPieceOutOfBounds() {
-
     const playField = this.playField;
-    const {x, y} = this.activePiece;
+    const { x, y } = this.activePiece;
 
-    // will return true if y is out of (0-20) range or if x is out of (0-10) range
+    // will return true if y is out of (0-19) range or if x is out of (0-9) range
     return playField[y] === undefined || playField[y][x] === undefined
+  }
+
+  
+  lockPiece() {
+    const { x: pieceX, y: pieceY, blocks } = this.activePiece;
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        this.playField[pieceY + y][pieceX + x] = blocks[y][x];
+      }
+    }
   }
 
 }
